e3b486accceb467ae2fb073717020b59e6703147
Ross Gray <rossagray@gmail.com>
2017-06-22 15:56:16 +0100

    Add some zsh scripts

---
+++ dotfiles/.oh-my-zsh/custom/dev.zsh
@@ -0,0 +1,7 @@
+# Go stuff
+export GOPATH=/Users/rossgray/dev/go
+export PATH=$PATH:$GOPATH/bin
+# gcloud stuff
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+source <(kubectl completion zsh)
