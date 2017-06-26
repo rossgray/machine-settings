2828b41fe24acbae5f899464aff9a56fec69a102
Ross Gray <rossagray@gmail.com>
2017-06-26 10:51:02 +0100

    Update GOPATH

--- dotfiles/.oh-my-zsh/custom/dev.zsh
+++ dotfiles/.oh-my-zsh/custom/dev.zsh
@@ -1,5 +1,5 @@
 # Go stuff
-export GOPATH=/Users/rossgray/dev/go
+export GOPATH=/Users/ross/dev/go
 export PATH=$PATH:$GOPATH/bin
 # gcloud stuff
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
