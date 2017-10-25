c97c41f5bb2daf57588acf79a0c0b3039a83595b
Ross Gray <rossagray@gmail.com>
2017-10-25 12:02:43 +0100

    Add kubernetes config to prompt

--- dotfiles/.oh-my-zsh/custom/dev.zsh
+++ dotfiles/.oh-my-zsh/custom/dev.zsh
@@ -5,3 +5,20 @@ export PATH=$PATH:$GOPATH/bin
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
 source <(kubectl completion zsh)
+
+kube_prompt() {
+	K8_CONTEXT=$(kubectl config current-context)
+	# Extract GCP metadata from the kubectl context name
+	if [[ $K8_CONTEXT == gke_* ]]; then
+		# kubectl context isn't a custom one
+		FULL_CONTEXT=$K8_CONTEXT
+	else
+		FULL_CONTEXT=$(kubectl config get-contexts $K8_CONTEXT | grep $K8_CONTEXT | awk '{print $3}'  )
+	fi
+	PROJECT=$(echo $FULL_CONTEXT | cut -d_ -f2)
+	REGION=$(echo $FULL_CONTEXT | cut -d_ -f3)
+	CLUSTER=$(echo $FULL_CONTEXT | cut -d_ -f4)
+	NS=$(kubectl config get-contexts $K8_CONTEXT --no-headers | awk '{print $5}')
+	kubectl_prompt="k8s:($PROJECT|$CLUSTER|$NS)"
+	echo $kubectl_prompt
+}
--- oh-my-zsh/rossgray.zsh-theme
+++ oh-my-zsh/rossgray.zsh-theme
@@ -3,6 +3,7 @@ local user='%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[green]%}%m:%{$reset_color%}'
 local pwd='%{$fg[yellow]%}%~%{$reset_color%}'
 local return_code='%(?..%{$fg[red]%}%? ↵%{$reset_color%})'
 local git_branch='$(git_prompt_info)%{$reset_color%}'
+local kube='$(kube_prompt)%{$reset_color%}'
 
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}["
 ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%}"
@@ -16,5 +17,5 @@ ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[magenta]%} ➜"
 ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[yellow]%} ═"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[cyan]%} ✭"
 
-PROMPT="${user}${pwd}$ "
-RPROMPT="${return_code} ${git_branch}"
+PROMPT="${user}${pwd} ${git_branch}$ "
+RPROMPT="${return_code} ${kube}"
