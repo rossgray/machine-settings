b072bf2c609f4014686d690fa12adb3511f18e04
Ross Gray <rossagray@gmail.com>
2018-09-11 16:13:07 +0100

    update runadmin command

--- dotfiles/.oh-my-zsh/custom/ingresso.zsh
+++ dotfiles/.oh-my-zsh/custom/ingresso.zsh
@@ -1,3 +1,3 @@
 alias runserver="sudo python manage.py runserver www.rossdev.ingresso.co.uk:80 --settings=whitelabel.settings.local"
-alias runadmin="python manage.py runserver www.rossdev.ingresso.co.uk:8000 --settings=services.settings.local"
+alias runadmin="python manage.py runserver www.rossdev.ingresso.co.uk:8000"
 alias rollback_wl="kubectl rollout undo deployment whitelabel"
