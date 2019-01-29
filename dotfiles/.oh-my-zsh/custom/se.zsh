95c098485240aaba2ee38ddb2b5161fe729b18c6
Ross Gray <rossagray@gmail.com>
2019-01-29 15:16:06 +0000

    Update se.zsh and iTerm prefs

--- dotfiles/.oh-my-zsh/custom/se.zsh
+++ dotfiles/.oh-my-zsh/custom/se.zsh
@@ -4,3 +4,6 @@ function lb() {
 	# edit weekly logbook file
 	$EDITOR /Volumes/GoogleDrive/My\ Drive/logbook/$(date -v 1w '+%Y-%m-%d').md
 }
+
+# Enable auto-completion for go-jira
+eval "$(jira --completion-script-zsh)"
