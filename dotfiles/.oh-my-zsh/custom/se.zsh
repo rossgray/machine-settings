c6ef5afe73da91fab6bf2aaf00c8db84099b7714
Ross Gray <ross.gray@secretescapes.com>
2019-12-05 11:21:41 +0000

    Add alias

--- dotfiles/.oh-my-zsh/custom/se.zsh
+++ dotfiles/.oh-my-zsh/custom/se.zsh
@@ -7,3 +7,6 @@ function lb() {
 
 # Enable auto-completion for go-jira
 eval "$(jira --completion-script-zsh)"
+
+alias up="bash deploy_env.sh -e dev_local -r up"
+alias down="bash deploy_env.sh -e dev_local -r down"
