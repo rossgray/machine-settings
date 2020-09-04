17a13cbc71147da8666c7be9abbbebea9e0981f2
Ross Gray <ross.gray@secretescapes.com>
2020-09-04 14:47:36 +0100

    Move alias

--- dotfiles/.oh-my-zsh/custom/se.zsh
+++ dotfiles/.oh-my-zsh/custom/se.zsh
@@ -13,3 +13,5 @@ alias down="bash deploy_env.sh -e dev_local -r down"
 
 alias start-ec2="aws ec2 start-instances --instance-ids i-0ba6f18d2f27fc19b"
 alias stop-ec2="aws ec2 stop-instances --instance-ids i-0ba6f18d2f27fc19b"
+
+alias tunnel="networksetup -connectpppoeservice 'VPN SE' && cd /Users/ross.gray/dev/one-data-pipeline && ./sbin/airflow_ssh_tunnel -e prod -u rossgray"
