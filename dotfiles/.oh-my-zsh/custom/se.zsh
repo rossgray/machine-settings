21bce0e64af67a4179163d2f8b5ad1b90cc34a09
Ross Gray <ross.gray@secretescapes.com>
2021-03-04 10:50:11 +0000

    Update se aliases

--- dotfiles/.oh-my-zsh/custom/se.zsh
+++ dotfiles/.oh-my-zsh/custom/se.zsh
@@ -8,10 +8,8 @@ function lb() {
 # Enable auto-completion for go-jira
 eval "$(jira --completion-script-zsh)"
 
-alias up="bash deploy_env.sh -e dev_local -r up"
-alias down="bash deploy_env.sh -e dev_local -r down"
-
 alias start-ec2="aws ec2 start-instances --instance-ids i-0ba6f18d2f27fc19b"
 alias stop-ec2="aws ec2 stop-instances --instance-ids i-0ba6f18d2f27fc19b"
 
 alias tunnel="networksetup -connectpppoeservice 'VPN SE' && cd /Users/ross.gray/dev/one-data-pipeline && ./sbin/airflow_ssh_tunnel -e prod -u rossgray"
+alias dagstatus="open http://localhost:8080/admin/dagrun/\?flt2_execution_date_greater_than\=$(date -v 2d -u +'%Y-%m-%d')+00%3A00%3A00\&flt4_state_not_equal\=success"
