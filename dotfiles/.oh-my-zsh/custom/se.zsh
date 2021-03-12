a0f179d8fcf063b6c5ac870396641c9a507ef905
Ross Gray <ross.gray@secretescapes.com>
2021-03-12 10:22:31 +0000

    fix alias

--- dotfiles/.oh-my-zsh/custom/se.zsh
+++ dotfiles/.oh-my-zsh/custom/se.zsh
@@ -12,4 +12,4 @@ alias start-ec2="aws ec2 start-instances --instance-ids i-0ba6f18d2f27fc19b"
 alias stop-ec2="aws ec2 stop-instances --instance-ids i-0ba6f18d2f27fc19b"
 
 alias tunnel="networksetup -connectpppoeservice 'VPN SE' && cd /Users/ross.gray/dev/one-data-pipeline && ./sbin/airflow_ssh_tunnel -e prod -u rossgray"
-alias dagstatus="open http://localhost:8080/admin/dagrun/\?flt2_execution_date_greater_than\=$(date -v 2d -u +'%Y-%m-%d')+00%3A00%3A00\&flt4_state_not_equal\=success"
+alias dagstatus="open http://localhost:8080/admin/dagrun/\?flt2_execution_date_greater_than\=$(date -v -2d -u +'%Y-%m-%d')+00%3A00%3A00\&flt4_state_not_equal\=success"
